e7ee78029e14e1ed3d1331e6408cb19a0a633925
Tyler Piscaglia <tpiscaglia1@yahoo.com>
2019-02-04 11:32:25 -0600

    Fix build issues
    Fixed build issues by adding mission_board_keyboard dll as project and removing references to non-existant files.

--- mission_board/Properties/Resources.Designer.cs
+++ mission_board/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace mission_board.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -96,26 +96,6 @@ namespace mission_board.Properties {
         internal static System.Drawing.Bitmap email3 {
             get {
                 object obj = ResourceManager.GetObject("email3", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap globe {
-            get {
-                object obj = ResourceManager.GetObject("globe", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap old_brown_paper_texture {
-            get {
-                object obj = ResourceManager.GetObject("old-brown-paper-texture", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
